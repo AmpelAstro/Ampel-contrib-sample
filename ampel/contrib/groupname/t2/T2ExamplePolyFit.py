16595cf2b20f47d699ae805e8610e5fe449d130a
vbrinnel <valery.brinnel@gmail.com>
2020-04-14 16:20:48 +0200

    T2ExamplePolyFit: v0.7 update

--- ampel/contrib/groupname/t2/T2ExamplePolyFit.py
+++ ampel/contrib/groupname/t2/T2ExamplePolyFit.py
@@ -4,67 +4,53 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 10.02.2018
-# Last Modified Date: 22.09.2018
+# Last Modified Date: 14.04.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from ampel.base.abstract.AmpelABC import AmpelABC, abstractmethod
-from ampel.base.abstract.AbsT2Unit import AbsT2Unit
 import numpy
+from typing import Union
+from ampel.view.LigthCurve import LigthCurve
+from ampel.t2.T2RunState import T2RunState
+from ampel.t2.T2UnitResult import T2UnitResult
+from ampel.abstract.AbsLightCurveT2Unit import AbsLightCurveT2Unit
 
-class T2ExamplePolyFit(AbsT2Unit):
+
+class T2ExamplePolyFit(AbsLightCurveT2Unit):
 	"""
-	Polynomial fitting
+	Polynomial fitting.
 	Fits data using numpy 'polyfit'
 	"""
 
-	version = 1.0
-	author = "ztf-software@desy.de"
+	version: float = 1.1
+	author: str = "ztf-software@desy.de"
+		
+	# Polynom degree used for numpy.polyfit 
+	degree: int = 5
 
-	def __init__(self, logger, base_config=dict()):
+	def run(self, lightcurve: LigthCurve) -> Union[T2UnitResult, T2RunState]:
 		"""
-		'logger': instance of logging.Logger (std python module 'logging')
-			-> example usage: logger.info("this is a log message")
-
-		'base_config': optional dict with keys given by the `resources` property of the class
-		"""
-
-		# Save the logger as instance variable
-		self.logger = logger
-
-	def run(self, light_curve, run_config):
-		"""
-		'light_curve': instance of ampel.base.LightCurve. See LightCurve docstring for more info.
-
-		'run_config': dict instance containing run parameters defined in ampel config section:
-			t2_run_config->POLYFIT_[run_config_id]->runConfig
-			whereby the run_config_id value is defined in the associated t2 document.
-			In the case of POLYFIT, run_config_id would be either 'default' or 'advanced'.
-			A given channel (say HU_SN_IA) could use the runConfig 'default' whereas
-			another channel (say OKC_SNIIP) could use the runConfig 'advanced'
-
-		This method must return either:
-			* A dict instance containing the values to be saved into the DB
-				-> IMPORTANT: the dict *must* be BSON serializable, that is:
-					import bson
-					bson.BSON.encode(<dict instance to be returned>)
-				must not throw a InvalidDocument Exception
-			* One of these T2RunStates flag member:
-				MISSING_INFO:  reserved for a future ampel extension where
-							   T2s results could depend on each other
-				BAD_CONFIG:	   Typically when run_config is not set properly
-				ERROR:		   Generic error
-				EXCEPTION:     An exception occured
+		:param light_curve: see LightCurve docstring for more info.
+		:returns: a dict instance containing the values to be saved into the DB
+		-> IMPORTANT: the dict *must* be BSON serializable, that is:
+			import bson
+			bson.BSON.encode(<dict instance to be returned>)
+		must not throw a InvalidDocument Exception
+		Alternatively, one of the following T2RunState (int) flag member can be retuned:
+		BAD_CONFIG, ERROR, EXCEPTION
 		"""
 
-		x = light_curve.get_values("obs_date")
-		y = light_curve.get_values("mag")
-		p = numpy.polyfit(x, y, run_config['degree'])
+		x = lightcurve.get_values("obs_date")
+		y = lightcurve.get_values("mag")
+		p = numpy.polyfit(x, y, self.degree)
 		chi_squared = numpy.sum((numpy.polyval(p, x) - y) ** 2)
 
 		self.logger.info("Please use 'self.logger' for logging")
 		self.logger.debug("By doing so, log entries will be automatically recorded into the database")
 
 		return {
-			"polyfit": list(p),
-			"chi2": numpy.sum((numpy.polyval(p, x) - y) ** 2)
+			"result": {
+				"polyfit": list(p),
+				"chi2": chi_squared
+			}
 		}
+	
